36f09d2017040cbb10e85d423390d1668385ad80
DeanSCND <54455314+DeanSCND-Symend@users.noreply.github.com>
2022-02-14 12:44:26 -0700

    Initial Commit

---
+++ SheeKitTest/SheeKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheeKitTestApp.swift
+//  SheeKitTest
+//
+//  Created by Dean on 2022-02-14.
+//
+
+import SwiftUI
+
+@main
+struct SheeKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
